ed2f132915d7afb462b26369633bc71103f676f4
STHSF <woailiyu@163.com>
2016-09-09 16:50:52 +0800

    code update

--- DeepNLP/sentiment_analysis/cs224d/datasets/get_datasets.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Get Stanford Sentiment Treebank
-wget http://nlp.stanford.edu/~socherr/stanfordSentimentTreebank.zip
-unzip stanfordSentimentTreebank.zip
-rm stanfordSentimentTreebank.zip
