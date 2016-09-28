7207a6e9e65aa8d9d0f5baffc0c0f83e59c23b15
STHSF <woailiyu@163.com>
2016-09-28 18:06:53 +0800

    code update

--- DeepNLP/sentiment_analysis_zh/word2vec.py
+++ DeepNLP/sentiment_analysis_zh/word2vec.py
@@ -10,25 +10,27 @@ import os
 
 # logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-# sentences = [['first', 'sentence'], ['second', 'sentence']]
-# # train word2vec on the two sentences
-# model = gensim.models.Word2Vec(sentences, min_count=1)
-
-
-class MySentences(object):
-    def __init__(self, dir_name):
-        self.dir_name = dir_name
-
-    def __iter__(self):
-        for file_name in os.listdir(self.dir_name):
-            for line in open(os.path.join(self.dir_name, file_name)):
-                yield line.split(",")
-
-
-sentences = MySentences('/Users/li/Kunyan/MyRepository/DeepNaturalLanguageProcessing/DeepNLP/data')  # a memory-friendly iterator
-model = gensim.models.Word2Vec(sentences, min_count=2, workers=4)
+sentences = [['中国', '人'], ['美国', '人']]
+# train word2vec on the two sentences
+model = gensim.models.Word2Vec(sentences, min_count=1)
 
 print model["中国"]
+
+
+
+# class MySentences(object):
+#     def __init__(self, dir_name):
+#         self.dir_name = dir_name
+#
+#     def __iter__(self):
+#         for file_name in os.listdir(self.dir_name):
+#             for line in open(os.path.join(self.dir_name, file_name)):
+#                 yield line.split(",")
+#
+#
+# sentences = MySentences('/Users/li/Kunyan/MyRepository/DeepNaturalLanguageProcessing/DeepNLP/data')  # a memory-friendly iterator
+# model = gensim.models.Word2Vec(sentences, min_count=2, workers=4)
+
 
 
 # model.save('/tmp/mymodel')
