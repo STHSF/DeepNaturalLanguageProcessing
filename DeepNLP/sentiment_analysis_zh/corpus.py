504781dd142ca0d674373982b5bd3b948fd0fb0d
Guadaler <zhangx_fh@163.com>
2016-10-10 18:30:41 +0800

    流程走到train步骤

--- DeepNLP/sentiment_analysis_zh/corpus.py
+++ DeepNLP/sentiment_analysis_zh/corpus.py
@@ -8,9 +8,12 @@
 
 import jieba
 import globe
+import os
+import sys
 
-input_txt = globe.input_txt
-output_txt = globe.output_txt
+reload(sys)
+sys.setdefaultencoding('utf8')
+
 
 def split_sentence(input_file, output_file):
     fin = open(input_file, 'r')  # 以读的方式打开文件
@@ -29,4 +32,51 @@ def split_sentence(input_file, output_file):
     fout.close()
 
 
-split_sentence(input_txt, output_txt)
+count = 0
+
+
+# 文本处理并写出
+def sentence(file_parent_path, out):
+    global count
+    for file_name in os.listdir(file_parent_path):
+        count += 1
+        print '正在处理：', count
+        file_path = os.path.join(file_parent_path, file_name)
+        data = open(file_path)
+        result = ""
+        for d in data:
+            temp = d.replace(" ", "").strip()
+            result += filter_stop_word(list(jieba.cut(temp)))
+        out.write(result + "\n")
+        out.flush()
+        data.close()
+
+
+# 过滤停用词
+def filter_stop_word(cut_result):
+    stopwords = {}.fromkeys([line.rstrip() for line in open(globe.stopword)])
+    final = []
+    for seg in cut_result:
+        seg = seg.encode('utf-8')
+        if seg not in stopwords:
+            final.append(seg)
+    final_str = ",".join(final)
+    return final_str
+
+
+def do():
+    input_txt = globe.input_txt
+    output_txt = globe.output_txt
+    # split_sentence(input_txt, output_txt)
+
+    out_neg = open("/home/zhangxin/work/DeepSentiment/data/tagging/result_neg.txt", "wb")  # 写出文件
+    out_neu = open("/home/zhangxin/work/DeepSentiment/data/tagging/result_neu.txt", "wb")  # 写出文件
+    out_pos = open("/home/zhangxin/work/DeepSentiment/data/tagging/result_pos.txt", "wb")  # 写出文件
+
+    sentence("/home/zhangxin/work/DeepSentiment/data/tagging/neg", out_neg)
+    sentence("/home/zhangxin/work/DeepSentiment/data/tagging/neu", out_neu)
+    sentence("/home/zhangxin/work/DeepSentiment/data/tagging/pos", out_pos)
+
+
+if __name__ == "__main__":
+    do()
