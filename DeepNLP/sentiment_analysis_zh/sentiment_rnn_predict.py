67616f0e7219e4f1ad1f26b79c18001114a57e97
STHSF <woailiyu@163.com>
2016-11-03 11:04:17 +0800

    code update

--- DeepNLP/sentiment_analysis_zh/sentiment_rnn_predict.py
+++ DeepNLP/sentiment_analysis_zh/sentiment_rnn_predict.py
@@ -9,22 +9,26 @@ import tensorflow as tf
 import input_data
 import globe
 import matplotlib.pyplot as plt
-
+import numpy as ny
 
 # set random seed for comparing the two result calculations
 tf.set_random_seed(1)
+
+# get data
+training_data = input_data.read_data_sets()
 
 # hyper_parameters
 lr = 0.001
 batch_size = 1
 
-n_inputs = 1  # data input size，输入层神经元
-n_steps = globe.n_dim  # time steps， w2v 维度
+n_inputs = 200  # data input size，输入层神经元
+n_steps = 1  # globe.n_dim  # time steps， w2v 维度
 n_hidden_units = 200  # neurons in hidden layer，隐藏层神经元个数
 n_classes = 2  # classes 二分类
 
 # tf Graph input
 x = tf.placeholder(tf.float32, [None, n_steps, n_inputs])
+y = tf.placeholder(tf.float32, [None, n_classes])
 
 # Define weights
 weights = {
@@ -89,17 +93,22 @@ def rnn(input_data, weights, biases):
 
     return results
 
+
 predict = rnn(x, weights, biases)
+
+correct_predict = tf.equal(tf.argmax(predict, 1), tf.argmax(y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_predict, tf.float32))
 
 saver = tf.train.Saver()
 
 with tf.Session() as sess:
-
     saver.restore(sess, globe.model_rnn_path)
 
     data = input_data.read_data_sets_predict()
 
+    # 单篇结果输出 注意修改 batch_size = 1
     for title in data.keys():
+
         batch_xs = data[title]
         batch_xs = batch_xs.reshape([batch_size, n_steps, n_inputs])
         result = sess.run([predict], feed_dict={x: batch_xs})
@@ -108,3 +117,40 @@ with tf.Session() as sess:
         print '【LabelIndex】', result[0].argmax()
         print '【result】', result[0].tolist()[0], '\n'
 
+    # 准确率输出 注意修改 batch_size = 7 (文章篇数)
+    # batch_ys = []
+    # batch_xs = []
+    #
+    # for title in data.keys():
+    #     batch_xs_temp = data[title]
+    #     batch_xs.append(batch_xs_temp)
+    #
+    #     if title.__contains__('neg'):
+    #         batch_ys.append((1, 0))
+    #     else:
+    #         batch_ys.append((0, 1))
+    #
+    #     print title
+    #
+    # batch_xs = ny.array(batch_xs)
+    # batch_xs = batch_xs.reshape([batch_size, n_steps, n_inputs])
+    #
+    # # 预测结果
+    # result = sess.run([predict], feed_dict={x: batch_xs})
+    # print result[0]
+    #
+    # # 精确度
+    # batch_ys = ny.array(batch_ys)
+    # batch_ys = batch_ys.reshape([-1, 2])
+    # acc = sess.run([accuracy], feed_dict={x: batch_xs, y: batch_ys})
+    # print 'Acc ', acc
+
+    # 原始测试集测试输出，batch_size 任意，可改可不改
+    # step = 0
+    # acc = []
+    # while step * batch_size < 600:
+    #     batch_xs, batch_ys = training_data.test.next_batch(batch_size)
+    #     batch_xs = batch_xs.reshape([batch_size, n_steps, n_inputs])
+    #     acc = sess.run([accuracy], feed_dict={x: batch_xs, y: batch_ys})
+    #     print 'Acc ', acc
+    #     step += 1
