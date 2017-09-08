f53c40eba8f66d4eb2a231178e5a88cf36df2575
STHSF <woailiyu@163.com>
2017-09-08 10:44:49 +0800

    2017-09-08-10

--- segmentation/main_processing.py
+++ segmentation/main_processing.py
@@ -1,7 +1,10 @@
 # coding=utf-8
+import time
 import pickle
+import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from data_generate import BatchGenerator
+from bidirectional_lstm import bi_lstm
 
 # 数据导入
 with open('data.pkl', 'rb') as pk:
@@ -27,6 +30,93 @@ data_test = BatchGenerator(X_test, y_test, shuffle=False)
 print 'Finished creating the data generator.'
 
 
+model = bi_lstm()
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+sess = tf.Session(config=config)
+
+sess.run(tf.global_variables_initializer())
+saver = tf.train.Saver(max_to_keep=10)  # 最多保存的模型数量
+
+decay = 0.85
+tr_batch_size = 128
+max_epoch = 5
+max_max_epoch = 6
+display_num = 5  # 每个 epoch 显示是个结果
+model_save_path = 'ckpt/bi-lstm.ckpt'  # 模型保存位置
+
+tr_batch_num = int(data_train.y.shape[0] / tr_batch_size)  # 每个 epoch 中包含的 batch 数
+display_batch = int(tr_batch_num / display_num)  # 每训练 display_batch 之后输出一次
+
+
+def test_epoch(dataset):
+    """Testing or valid."""
+    _batch_size = 500
+    fetches = [model.accuracy, model.loss]
+    _y = dataset.y
+    data_size = _y.shape[0]
+    batch_num = int(data_size / _batch_size)
+    start_time = time.time()
+    _costs = 0.0
+    _accs = 0.0
+    for i in xrange(batch_num):
+        X_batch, y_batch = dataset.next_batch(_batch_size)
+        feed_dict = {model.source_inputs: X_batch,
+                     model.target_inputs: y_batch,
+                     model.lr: 1e-5,
+                     model.batch_size: _batch_size,
+                     model.keep_prob: 1.0}
+        _acc, _cost = sess.run(fetches, feed_dict)
+        _accs += _acc
+        _costs += _cost
+    mean_acc = _accs / batch_num
+    mean_cost = _costs / batch_num
+    return mean_acc, mean_cost
+
+
+for epoch in xrange(max_max_epoch):
+    _lr = 1e-4
+    if epoch > max_epoch:
+        _lr = _lr * (decay ** (epoch - max_epoch))
+    print 'EPOCH %d， lr=%g' % (epoch + 1, _lr)
+    start_time = time.time()
+    _costs = 0.0
+    _accs = 0.0
+    show_accs = 0.0
+    show_costs = 0.0
+    for batch in xrange(tr_batch_num):
+        fetches = [model.accuracy, model.loss, model.train_op]
+        X_batch, y_batch = data_train.next_batch(tr_batch_size)
+        feed_dict = {model.source_inputs: X_batch,
+                     model.target_inputs: y_batch,
+                     model.lr: _lr,
+                     model.batch_size: tr_batch_size,
+                     model.keep_prob: 0.5}
+        _acc, _cost, _ = sess.run(fetches, feed_dict)  # the cost is the mean cost of one batch
+        _accs += _acc
+        _costs += _cost
+        show_accs += _acc
+        show_costs += _cost
+        if (batch + 1) % display_batch == 0:
+            valid_acc, valid_cost = test_epoch(data_valid)  # valid
+            print '\ttraining acc=%g, cost=%g;  valid acc= %g, cost=%g ' % (show_accs / display_batch,
+                                                                            show_costs / display_batch, valid_acc,
+                                                                            valid_cost)
+            show_accs = 0.0
+            show_costs = 0.0
+    mean_acc = _accs / tr_batch_num
+    mean_cost = _costs / tr_batch_num
+    if (epoch + 1) % 3 == 0:  # 每 3 个 epoch 保存一次模型
+        save_path = saver.save(sess, model_save_path, global_step=(epoch + 1))
+        print 'the save path is ', save_path
+    print '\ttraining %d, acc=%g, cost=%g ' % (data_train.y.shape[0], mean_acc, mean_cost)
+    print 'Epoch training %d, acc=%g, cost=%g, speed=%g s/epoch'\
+          % (data_train.y.shape[0], mean_acc, mean_cost, time.time() - start_time)
+
+# testing
+print '**TEST RESULT:'
+test_acc, test_cost = test_epoch(data_test)
+print '**Test %d, acc=%g, cost=%g' % (data_test.y.shape[0], test_acc, test_cost)
 
 
 
