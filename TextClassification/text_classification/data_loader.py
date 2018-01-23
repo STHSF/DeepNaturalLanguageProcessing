c3638a1e9914bb993971d51b5ffd70d13e7ced40
sthsf <liyu52577@163.com>
2018-01-23 08:48:08 +0800

    2018-01-23-08

--- TextClassification/text_classification/data_loader.py
+++ TextClassification/text_classification/data_loader.py
@@ -1,6 +1,12 @@
 # coding=utf-8
 
 import os
+import sys
+import numpy as np
+from collections import Counter
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
 
 
 class config(object):
@@ -11,19 +17,43 @@ class config(object):
 
 def read_file(file_path):
     """
-    读取文件被容
+    读取文件内容，并将文件中的label和content分开存储
     :param file_path:
     :return:
     """
-    labels, datas = [], []
-    with open(file_path) as f:
+    labels, contents = [], []
+    with open(file_path, 'r') as f:
         for line in f.readlines():
             try:
                 label, data = line.strip().split('\t')
                 labels.append(label)
-                datas.append(data)
+                contents.append(data)
             except:
                 pass
-    return labels, datas
+    return labels, contents
+
+
+def build_vocab(file_patch, vocab_size=5000):
+    _, contents = read_file(file_patch)
+    all_data = []
+    # 拼接
+    for content in contents:
+        all_data.extend(list(content.decode('utf-8')))
+    # 统计中文字符出现的次数
+    counter = Counter(all_data)
+    # 挑选前vocab_size个中文字符
+    counter_dict = counter.most_common(vocab_size - 1)
+
+    words, _ = list(zip(*counter_dict))
+    # 添加一个 <PAD> 来将所有文本pad为同一长度
+    words = ['<PAD>'] + list(words)
+
+    # 将筛选出来的中文字符写入文件
+    with open('rr.txt', 'w') as f:
+        f.write('\n'.join(words) + '\n')
+
+
+build_vocab(config.val_file_patch)
+
 
 
