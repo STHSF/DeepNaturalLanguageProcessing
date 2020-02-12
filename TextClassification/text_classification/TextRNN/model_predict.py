30d1f0f3d8c4484a51c3636547639783913abe6b
li <liyu52577@163.com>
2020-02-12 22:32:51 +0800

    code update

--- TextClassification/text_classification/TextRNN/model_predict.py
+++ TextClassification/text_classification/TextRNN/model_predict.py
@@ -29,7 +29,7 @@ except NameError:
 base_dir = '../data/cnews'
 vocab_dir = os.path.join(base_dir, 'cnews.vocab.txt')
 
-save_dir = '../checkpoints/textbirnn'
+save_dir = './checkpoints/textbirnn'
 save_path = os.path.join(save_dir, 'best_validation')  # 最佳验证结果保存路径
 
 
